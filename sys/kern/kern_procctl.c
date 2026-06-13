0b1287af6f5cd2a9473260d1bda7573998216462
Shawn Webb <shawn.webb@hardenedbsd.org>
2026-06-13 13:56:52 +0000

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- sys/kern/kern_procctl.c
+++ sys/kern/kern_procctl.c
@@ -931,11 +931,8 @@ wxmap_status(struct thread *td, struct proc *p, void *data)
 		vmspace_free(vm);
 	}
 	*(int *)data = d;
-<<<<<<< HEAD
-#endif
-=======
 	PROC_LOCK(p);
->>>>>>> upstream/stable/15
+#endif
 	return (0);
 }
 
