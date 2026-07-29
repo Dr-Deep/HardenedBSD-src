c4d5dde0b1c4b8558f79f49fa289f9d79d4cec19
Shawn Webb <shawn.webb@hardenedbsd.org>
2026-07-29 21:45:03 +0000

    HBSD: Provide support for the !PAX_HARDENING case
    I doubt HardenedBSD has any users with PAX_HARDENING disabled, but here
    we are.
    
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>
    Fixes:		40e739dfc16a38af0346865a5f6253b7e7974a50
    MFC-to:		15-STABLE

--- sys/geom/geom_ctl.c
+++ sys/geom/geom_ctl.c
@@ -72,6 +72,9 @@ g_ctl_init(void)
 #ifdef PAX_HARDENING
 	make_dev_credf(MAKEDEV_ETERNAL, &g_ctl_cdevsw, 0, NULL,
 	    UID_ROOT, GID_WHEEL, 0600, PATH_GEOM_CTL);
+#else
+	make_dev_credf(MAKEDEV_ETERNAL, &g_ctl_cdevsw, 0, NULL,
+	    UID_ROOT, GID_OPERATOR, 0640, PATH_GEOM_CTL);
 #endif
 }
 
